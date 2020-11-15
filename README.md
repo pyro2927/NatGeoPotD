7e7a8eee1feea8b8a5a71e35bfd14f2dfdea2c9f
Joseph Pintozzi <joseph@pintozzi.com>
2020-11-14 22:37:25 -0600

    Adding readme, going into history

---
+++ README.md
@@ -0,0 +1,17 @@
+# NatGeoPotD
+
+Download an archive of the National Geographic Photo of the Day(s).
+
+## Setup
+
+```bash
+$ pyenv install
+$ pip isntall -r requirements.txt
+```
+
+
+## Run
+
+```bash
+$ python ./download.py
+```
--- download.py
+++ download.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
+from PIL import Image
+from datetime import date
 from datetime import datetime
+from re import search
 import json
 import os
 import pathlib
+import piexif
 import requests
 
 def get_galary_json(year, month):
     year_month = "{}-{}".format(year, month)
+    print("Downloading {}".format(year_month))
     base_url = 'https://www.nationalgeographic.com'
     json_url = base_url + '/content/photography/en_US/photo-of-the-day/_jcr_content/.gallery.' + year_month + '.json'
 
@@ -20,14 +25,41 @@ def download_potd(photo_destination, photo_url):
 ## iterate through photos - and download only ones which are missing
 def download_the_photos(natgeo_json_data):
     for photo in natgeo_json_data["items"]:
-        if "uri" in photo["image"]:
-            d = datetime.strptime(photo["publishDate"], "%B %d, %Y")
-            photo_url = photo["image"]["uri"]
-            folder = d.strftime("%Y/%m")
-            dest = d.strftime("%Y/%m/%d.jpg")
-            pathlib.Path(folder).mkdir(parents=True, exist_ok=True)
-            download_potd(dest, photo_url)
+        d = datetime.strptime(photo["publishDate"], "%B %d, %Y")
+        folder = d.strftime("%Y/%m")
+        dest = d.strftime("%Y/%m/%d.jpg")
+        pathlib.Path(folder).mkdir(parents=True, exist_ok=True)
+        # Skip downloading if we already have it
+        if not os.path.exists(dest):
+            if "uri" in photo["image"]:
+                photo_url = photo["image"]["uri"]
+                download_potd(dest, photo_url)
+        # set exif data
+        im = Image.open(dest)
+        try:
+            exif_dict = piexif.load(im.info["exif"])
+        except:
+            exif_dict = {"0th": {}}
+        exif_dict["0th"][piexif.ImageIFD.XPTitle] = bytearray(photo["image"]["title"], 'utf-8')
+        exif_dict["0th"][piexif.ImageIFD.ImageDescription] = bytearray(photo["image"]["caption"], 'utf-8')
+        try:
+            # Being fancy with regex to account for typos on NatGeo's side
+            author = search("(?i)Photograp?hy? by (.*)(, Nat|$)", photo["image"]["credit"]).group(1)
+            exif_dict["0th"][piexif.ImageIFD.XPAuthor] = bytearray(author, 'utf-16')
+        except:
+            print("Unable to pull author name from {}".format(photo["image"]["credit"]))
+        # write exif back
+        exif_bytes = piexif.dump(exif_dict)
+        im.save(dest, "jpeg", exif=exif_bytes)
 
 if __name__ == '__main__':
-    j = get_galary_json(2020, 11)
-    download_the_photos(j)
+    dt = date.today()
+    y = dt.year
+    m = dt.month
+    while True:
+        j = get_galary_json(y, m)
+        download_the_photos(j)
+        m = m - 1
+        if m == 0:
+            m = 12
+            y = y - 1
